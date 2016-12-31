1632bf87f45af39fd303620f30b690bf4e3a9116
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2016-12-31 10:44:05 +0100

    Improve piano looks

--- ui/Piano.jsx
+++ ui/Piano.jsx
@@ -3,43 +3,84 @@ const React = require('react');
 
 class Piano extends React.Component {
 
-  constructor() {
-    super();
-    if(!Piano.IMAGE_CACHE) {
-      Piano.IMAGE_CACHE = {};
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-      canvas.width = 650;
-      canvas.height = 20;
-      ctx.fillStyle = 'white';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      const width = 5;
-      ctx.fillStyle = 'black';
-      for(let x = 0; x < canvas.width; x += width) {
-        ctx.fillRect(x, 0, 1, canvas.height);
-      }
-
-      let i = 0;
-      for(let x = 0; x < canvas.width; x += width) {
-        i++;
-        if(i % 7 == 0 || i % 7 == 3) {
-          continue;
-        }
-        ctx.fillRect(x + 4, 0, 3, canvas.height / 5 * 3);
-      }
-      Piano.IMAGE_CACHE.piano = canvas.toDataURL();
-    }
-  }
-
   componentDidMount() {
     this.ctx = this.canvas.getContext('2d');
+    this.topCtx = this.topCanvas.getContext('2d');
     this.renderLoop();
   }
 
   renderLoop() {
     requestAnimationFrame(() => this.renderLoop());
-    const width = 5;
-    const innerWidth = width - 1;
+    const width = 12;
+    const innerWidth = width + 1;
+    const noteOffset = 36 + 3;
+
+    this.topCtx.fillStyle = 'black';
+    this.topCtx.fillRect(0, 0, this.topCanvas.width, this.topCanvas.height);
+    this.topCtx.strokeStyle = '#ddd';
+    let i = -1;
+    const activeNoteNumbers = {}
+    for(let i = 0; i < this.props.channel.activeNotesCount; i++) {
+      let note = this.props.channel.notes[i]; 
+      activeNoteNumbers[note.note] = true;
+    }
+    for(let x = 0; x < this.topCanvas.width; x += width) {
+      let offset = 0;
+      let padding = 0;
+      i++;
+      switch(i % 12) {
+        case 0:
+          offset = 0;
+          padding = width / 2;
+          break;
+        case 1:
+        case 3:
+        case 6:
+        case 8:
+        case 10:
+          continue;
+        case 2:
+        case 7:
+        case 9:
+          offset = -width / 2;
+          padding = width;
+          break;
+        case 4:
+        case 11:
+          offset = -width / 2;
+          padding = width / 2;
+          break;
+        case 5:
+          offset = 0;
+          padding = width / 2;
+          break;
+      }
+      this.topCtx.fillStyle = i % 12 ? '#e7ebf2' : '#c6cad1';
+      if((i + noteOffset) in activeNoteNumbers) {
+        this.topCtx.fillStyle = '#ffca8e';
+      }
+      this.topCtx.fillRect(x + offset, 0, width + padding, this.topCanvas.height);
+      this.topCtx.strokeRect(x + offset, 0, width + padding, this.topCanvas.height);
+    }
+
+    i = -1;
+    for(let x = 0; x < this.topCanvas.width; x += width) {
+      i++;
+      switch(i % 12) {
+        case 1:
+        case 3:
+        case 6:
+        case 8:
+        case 10:
+        if((i + noteOffset) in activeNoteNumbers) {
+          this.topCtx.fillStyle = '#fe8025';
+        } else {
+          this.topCtx.fillStyle = '#3c3d3f';
+        }
+        this.topCtx.fillRect(x, 0, width, this.topCanvas.height / 5 * 3);
+      }
+    }
+
     this.ctx.drawImage(this.canvas, 0, 1);
     this.ctx.fillStyle = '#253237';
     this.ctx.fillRect(0, 0, this.canvas.width, 1);
@@ -51,18 +92,24 @@ class Piano extends React.Component {
     this.ctx.globalAlpha = 1;
     for(let i = 0; i < this.props.channel.activeNotesCount; i++) {
       let note = this.props.channel.notes[i]; 
-      this.ctx.fillRect((note.note - 36) * width, 0, innerWidth / 2 + note.velocity / 127 * innerWidth, 1);
+      if(note.releaseTime != -1) {
+        this.ctx.fillStyle = 'grey';
+      } else {
+        this.ctx.fillStyle = '#abdab4';
+      }
+      this.ctx.fillRect((note.note - noteOffset) * width + width / 2 - note.gain.gain.value * innerWidth / 2, 0, note.gain.gain.value * innerWidth, 1);
     }
   }
 
   render() {
     return (
       <div>
-        <div style={{
-          backgroundImage: Piano.IMAGE_CACHE ? 'url(' + Piano.IMAGE_CACHE.piano + ')' : '',
-          width: '650px',
-          height: '20px'
-          }}/>
+        <canvas
+          className="top-piano"
+          ref={canvas => this.topCanvas = canvas}
+          width="650"
+          height="40"
+          />
         <canvas
           className="piano"
           ref={canvas => this.canvas = canvas}
